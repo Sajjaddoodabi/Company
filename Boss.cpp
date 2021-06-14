7faaf4efe44aab0d357396aa69d405a686552544
sajjad <sajjaddoodabi55@gmail.com>
2021-06-14 15:41:12 +0430

    Boss constructors

--- Boss.cpp
+++ Boss.cpp
@@ -1,5 +1,15 @@
-//
-// Created by sajja on 6/14/2021.
-//
-
 #include "Boss.h"
+
+Boss::Boss() : Employee() {
+    numberOfEmployees = 0;
+}
+
+Boss::Boss(const string &name, const string &id, const Address &address1, int hw, int sph, int wtd, int wd, int noe)
+        : Employee(name, id,address1,  hw,  sph,  wtd,  wd) {
+
+    numberOfEmployees = noe;
+}
+
+Boss::Boss(const Boss &boss) : Employee(boss) {
+    numberOfEmployees = boss.numberOfEmployees;
+}
--- Boss.h
+++ Boss.h
@@ -6,6 +6,13 @@
 class Boss : public Employee {
 private:
     int numberOfEmployees;
+public:
+    //constructors
+    Boss();
+    Boss(const string & name , const string & id , const Address &address1 , int hw , int sph , int wtd , int wd , int noe);
+
+    //copy constructor
+    Boss(const Boss &);
 };
 
 
