e06838cdf3a7188d15d4ce7d47d7eb889685b5f7
sajjad <sajjaddoodabi55@gmail.com>
2021-06-14 15:27:59 +0430

    Employee values

---
+++ Employee.h
@@ -0,0 +1,15 @@
+#ifndef COMPANY_EMPLOYEE_H
+#define COMPANY_EMPLOYEE_H
+
+#include "Person.h"
+
+class Employee {
+private:
+    int hourWork;
+    int salaryPerHour;
+    int workToDo;
+    int workDone;
+};
+
+
+#endif //COMPANY_EMPLOYEE_H
