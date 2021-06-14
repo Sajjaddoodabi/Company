a4d71e02b833ff8771fcbfced2398346e1d595b0
sajjad <sajjaddoodabi55@gmail.com>
2021-06-14 15:05:16 +0430

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Company.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Company.dir/main.cpp.obj"
+  "Company.exe"
+  "Company.exe.manifest"
+  "Company.pdb"
+  "libCompany.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Company.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Company.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Company.
+# This may be replaced when dependencies are built.
